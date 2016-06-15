7a56e3465b662654251f8235bc708c14ddb56ea0
Marvin Huebner <mjh@bergwerk.ag>
2016-06-15 23:50:15 +0200

    adjust warning massages

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 'use strict';
 
 var fs = require('fs'),
-    assign = require('object-assign');
+    assign = require('object-assign'),
+    gutil = require('gulp-util');
 
 
 function checkModuleVersion(options) {
     if (typeof options === 'undefined') {
-        throw new Error('Check requires a settings object.');
+        throw new Error('It requires a settings object to compare the modules');
     }
 
     if (typeof options.module === 'undefined') {
-        throw new Error('You need to asign a module');
+        throw new Error('You need to assign a module as a string');
     }
 
     options = assign({
         dependencies: false,
         devDependencies: false,
-        module: null
+        module: ''
     }, options);
 
+    if (typeof options.module !== 'string') {
+        throw new Error('option:module must be a string')
+    }
+
     if (typeof options.dependencies !== 'boolean') {
-        throw new Error('dependencies must be a boolean');
+        throw new Error('option:dependencies must be a boolean');
     }
 
     if (typeof options.devDependencies !== 'boolean') {
-        throw new Error('devDependencies must be a boolean');
+        throw new Error('option:devDependencies must be a boolean');
     }
 
     if (options.dependencies == true && options.devDependencies == true) {
@@ -49,7 +54,7 @@ function checkModuleVersion(options) {
     var moduleVerson = modulePackageJson.version;
 
     if (projectVersionOnly != moduleVerson) {
-        throw new Error('You hove not the same versions');
+        throw new Error(gutil.colors.red('ERROR: ') + gutil.colors.cyan(options.module) + ' version is not equal to ' + gutil.colors.cyan(options.module) + ' in your node_modules folder\n\n');
     }
 }
 
